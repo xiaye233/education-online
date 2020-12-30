659af99d94ffdb9d4bfc69d494d6de0a1478e5e4
xiaye233 <xy15190632656@163.com>
2020-12-30 14:02:51 +0800

    Initial commit

---
+++ com_atguigu_sparkstreaming/src/main/scala/com/atguigu/qzpoint/bean/Models.scala
@@ -0,0 +1,16 @@
+package com.atguigu.qzpoint.bean
+
+case class LearnModel(
+                       userId: Int,
+                       cwareId: Int,
+                       videoId: Int,
+                       chapterId: Int,
+                       edutypeId: Int,
+                       subjectId: Int,
+                       sourceType: String,
+                       speed: Int,
+                       ts: Long,
+                       te: Long,
+                       ps: Int,
+                       pe: Int
+                     )
